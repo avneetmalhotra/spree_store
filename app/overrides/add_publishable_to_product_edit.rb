02f162183b40310c7ac241c3ddbdef106a6b5df1
Avneet Singh Malhotra <avneet@vinsol.com>
2018-09-13 12:57:17 +0530

    cr2 changes

--- app/overrides/add_publishable_to_product_edit.rb
+++ app/overrides/add_publishable_to_product_edit.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(virtual_path: 'spree/admin/products/_form',
                      name: 'add_publishable_to_products_edit',
                      insert_before: "[data-hook='admin_product_form_promotionable']",
-                     partial: '../views/spree/admin/products/publishable_field'
+                     partial: 'spree/admin/products/publishable_field'
                     )
